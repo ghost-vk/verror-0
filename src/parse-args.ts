36e17ce9ab7bae162b3e8c77e5059ebc2c4b3a20
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-12-30 20:11:44 +0300

    chore: small refactor

--- src/parse-args.ts
+++ src/parse-args.ts
@@ -28,33 +28,33 @@ export function parseArgs(args: Args) {
   const options: {
     cause?: unknown;
   } = {};
-  let sprintf_args: unknown[] = [];
+  let sprintfArgs: unknown[] = [];
   // Определим какая форма вызова используется.
   if (argv.length === 0) {
     // Тут ничего делать не нужно.
   } else if (isError(argv[0])) {
     options.cause = argv[0];
     // Слайсим все элементы как аргументы sprintf после причины/ошибки.
-    sprintf_args = argv.slice(1);
+    sprintfArgs = argv.slice(1);
   } else if (typeof argv[0] === 'object' && argv[0] !== null) {
     for (const k in argv[0]) {
       options[k] = argv[0][k];
     }
-    sprintf_args = argv.slice(1);
+    sprintfArgs = argv.slice(1);
   } else {
     if (typeof argv[0] !== 'string') {
       throw new Error('first argument to VError or WError constructor must be a string, object, or Error');
     }
     // В случае когда передана строка - все аргументы идут для sprintf.
-    sprintf_args = argv;
+    sprintfArgs = argv;
   }
 
   let shortmessage: string;
-  if (sprintf_args.length === 0) {
+  if (sprintfArgs.length === 0) {
     shortmessage = '';
   } else {
     // 1й - message, остальные - параметры.
-    shortmessage = format(...sprintf_args);
+    shortmessage = format(...sprintfArgs);
   }
 
   return {
