cd332c062094d3c6ad3e82fa93dae016f4a15680
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-12-31 14:31:33 +0300

    test: add undefined & null test

--- src/__tests__/verror.spec.ts
+++ src/__tests__/verror.spec.ts
@@ -8,4 +8,14 @@ describe('verror test', () => {
     assert(verr, 'should be defined');
     assert.equal(verr.message, 'app message with param someValue: db error');
   });
+
+  it('handles undefined as string arg', () => {
+    const err = new VError('undefined as string => %s', undefined);
+    assert.equal(err.message, 'undefined as string => undefined');
+  });
+
+  it('handles null as string arg', () => {
+    const err = new VError('null as string => %s', null);
+    assert.equal(err.message, 'null as string => null');
+  });
 });
