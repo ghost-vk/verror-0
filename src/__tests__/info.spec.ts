ab7a296f3725a07b695e0533d2f04054b40903de
Vlad Kuznetsov <ghost.vkv@gmail.com>
2025-01-01 22:08:33 +0300

    test: info

---
+++ src/__tests__/info.spec.ts
@@ -0,0 +1,80 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { VError } from '../verror.js';
+
+describe('info test', () => {
+  const err1 = new VError(
+    {
+      name: 'MyError',
+      info: {
+        errno: 'EDEADLK',
+        anobject: { hello: 'world' }
+      }
+    },
+    'bad'
+  );
+
+  it('simple info usage', () => {
+    assert.equal(err1.name, 'MyError');
+    assert.deepEqual(VError.info(err1), {
+      errno: 'EDEADLK',
+      anobject: { hello: 'world' }
+    });
+  });
+
+  it('should propagate info from nested errors', () => {
+    const err2 = new VError(err1, 'worse');
+    assert.equal(VError.cause(err2), err1);
+    assert.deepEqual(VError.info(err2), {
+      errno: 'EDEADLK',
+      anobject: { hello: 'world' }
+    });
+  });
+
+  it('should override property', () => {
+    const err2 = new VError(
+      {
+        cause: err1,
+        info: {
+          anobject: { hello: 'moon' }
+        }
+      },
+      'worse'
+    );
+    assert.equal(VError.cause(err2), err1);
+    assert.equal(err2.message, 'worse: bad');
+    assert.deepEqual(VError.info(err2), {
+      errno: 'EDEADLK',
+      anobject: { hello: 'moon' }
+    });
+  });
+
+  it('should add a third-level to the chain', () => {
+    const err2 = new VError(
+      {
+        cause: err1,
+        info: {
+          anobject: { hello: 'moon' }
+        }
+      },
+      'worse'
+    );
+    const err3 = new VError(
+      {
+        cause: err2,
+        name: 'BigError',
+        info: {
+          remote_ip: '127.0.0.1'
+        }
+      },
+      'what next'
+    );
+    assert.equal(err3.name, 'BigError');
+    assert.equal(VError.cause(err3), err2);
+    assert.deepEqual(VError.info(err3), {
+      errno: 'EDEADLK',
+      anobject: { hello: 'moon' },
+      remote_ip: '127.0.0.1'
+    });
+  });
+});
