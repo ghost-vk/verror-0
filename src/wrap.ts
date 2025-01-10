6d2f19102afb9db1ac010cff7f6aa5ff6e6df098
Vlad Kuznetsov <ghost.vkv@gmail.com>
2025-01-10 07:25:10 +0300

    docs: update fn description

--- src/wrap.ts
+++ src/wrap.ts
@@ -2,20 +2,16 @@ import { VError } from './verror.js';
 import { ofList } from './of-list.js';
 
 /**
- * Функция оборачивает `unknown` в `Error`. Обрабатывает случай вложенных ошибок (`AggregateError`).
+ * Wraps `unknown` to `Error`. Handles nested errors (`AggregateError` case).
  *
  * @example
- * // import VError from 'verror'
- * // import { wrap } from '@mc-edu/node-utils'
- *
  * try {
  *   // Throws error with message "database connection error"
  *   await registerTicket(10001000)
  * } catch (err) {
- *   const cause = wrap(err)
  *   // Result message:
  *   // "Error register ticket 10001000: database connection error"
- *   throw new VError(cause, 'Error register ticket %s', 10001000)
+ *   throw new VError(wrap(err), 'Error register ticket %s', 10001000)
  * }
  */
 export function wrap(err: unknown): Error {
